a55e487fab62036b89e08ac76b12618d5839d985
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-02-21 19:51:11 +0100

    Add main.js to root for testing

---
+++ main.js
@@ -0,0 +1,35 @@
+function fetchHeaderAndFooter() {
+    document.addEventListener("DOMContentLoaded", function () {
+        fetch('PageComponents/header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header').innerHTML = data;
+                executeScripts(data);
+            });
+        fetch('PageComponents/footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer').innerHTML = data;
+                executeScripts(data);
+            });
+    })
+}
+
+function executeScripts(html) {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = html;
+    const scripts = tempDiv.querySelectorAll('script');
+    
+    scripts.forEach(script => {
+        const newScript = document.createElement('script');
+        if (script.src) {
+            newScript.src = script.src;
+        } else {
+            newScript.textContent = script.textContent;
+        }
+        document.head.appendChild(newScript);
+        document.head.removeChild(newScript);
+    });
+}
+
+fetchHeaderAndFooter();
