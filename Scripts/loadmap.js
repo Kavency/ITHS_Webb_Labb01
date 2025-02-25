bd3f0a31da596779d915aa4825e8fe6d7aad3d39
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-02-25 15:53:58 +0100

    Update about page

--- Scripts/loadmap.js
+++ Scripts/loadmap.js
@@ -2,8 +2,8 @@ var map = document.getElementById ('map');
 var mapListener = function (e) { 
     var frame = document.createElement ('iframe'); 
     frame.src = this.getAttribute ('data-src');
-    frame.width = 600;
-    frame.height = 450;
+    // frame.width = 600;
+    // frame.height = 450;
     map.appendChild (frame); 
     map.removeEventListener ("mouseover", mapListener); 
 }; 
